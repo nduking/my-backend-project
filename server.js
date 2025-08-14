fe329f0615bf66b236aae70be8e90cecc62958bc
nduking <114291880+nduking@users.noreply.github.com>
2025-08-14 13:08:17 +0100

    prot number

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+//Enable usage of .env files -This must always be at the top-mst part of your server/app/index .js file
 require("dotenv").config();
 
 //Import routes
