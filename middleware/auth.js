285fe8a1a56631e1af0504243a1cf15e84e54cf6
Julius Adebowale <juadebgabriel@gmail.com>
2025-08-21 19:54:49 +0100

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User.model");
+const User = require("../models/User");
 
 const auth = async (req, res, next) => {
   try {
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,25 @@
-const express=require('express');
-const {register,login,getProfile}=require('../controllers/authController');
-const auth=require('../middleware/auth');
+const express = require("express");
+// const {
+//   register,
+//   login,
+//   getProfile,
+// } = require("../controllers/authController"); 1st issue is here
 
+const {
+  register,
+  login,
+  getProfile,
+} = require("../controllers/authControllers");
 
-const router=express.Router();
+const auth = require("../middleware/auth");
+
+const router = express.Router();
 
 //Public routes (no authentication required)
-router.post('/register',register);
-router.post('/login',login);
+router.post("/register", register);
+router.post("/login", login);
 
 //Protected routes (authentication required)
-router.get('/profile',auth,getProfile);
+router.get("/profile", auth, getProfile);
 
-module.exports=router;
+module.exports = router;
